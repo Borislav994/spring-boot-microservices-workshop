d5844582e408a71efc0c90dff378991a1c0a1aec
Borislav994 <borislavspasic2609@gmail.com>
2024-01-29 14:51:14 +0100

    Finished level 2 of Java Brain Microservices series. Added Hystrix and example how to configure Bulkhead Pattern

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/CatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/CatalogResource.java
@@ -1,20 +1,14 @@
 package io.javabrains.moviecatalogservice.resources;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import io.javabrains.moviecatalogservice.models.CatalogItem;
-import io.javabrains.moviecatalogservice.models.Movie;
-import io.javabrains.moviecatalogservice.models.Rating;
 import io.javabrains.moviecatalogservice.models.UserRating;
+import io.javabrains.moviecatalogservice.services.MovieInfo;
+import io.javabrains.moviecatalogservice.services.UserRatingInfo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -23,35 +17,36 @@ import java.util.stream.Collectors;
 public class CatalogResource {
 
     @Autowired
-    private RestTemplate restTemplate;
+    MovieInfo movieInfo;
+
+    @Autowired
+    UserRatingInfo userRatingInfo;
+
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+        UserRating userRating = userRatingInfo.getUserRating(userId);
+        return userRating.getRatings().stream()
+                .map(rating -> movieInfo.getCatalogItem(rating))
+                .collect(Collectors.toList());
+    }
+
+
+
+
+
+//    public List<CatalogItem> getFallbackCatalog(@PathVariable("userId") String userId) {
+//        return Collections.singletonList(new CatalogItem("No movie", "", 0));
+//    }
 
 //    @Autowired
 //    private DiscoveryClient discoveryClient; ovo je za advance load balancing i think
 
-    @Autowired
-    WebClient.Builder webClientBuilder;
+//    Alternative WebClient way
+//    Movie movie = webClientBuilder.build().get().uri("http://localhost:8082/movies/"+ rating.getMovieId())
+//    .retrieve().bodyToMono(Movie.class).block();
 
-    @RequestMapping("/{userId}")
-    @HystrixCommand(fallbackMethod = "getFallbackCatalog")
-    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+//    @Autowired
+//    WebClient.Builder webClientBuilder;
 
-        UserRating userRating = restTemplate.getForObject("http://ratings-data-service/ratingsdata/user/" + userId, UserRating.class);
-
-        return userRating.getRatings().stream()
-                .map(rating -> {
-                    Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
-                    return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
-                })
-                .collect(Collectors.toList());
-    }
-
-    public List<CatalogItem> getFallbackCatalog(@PathVariable("userId") String userId) {
-        return Collections.singletonList(new CatalogItem("No movie", "", 0));
-    }
 }
 
-/*
-Alternative WebClient way
-Movie movie = webClientBuilder.build().get().uri("http://localhost:8082/movies/"+ rating.getMovieId())
-.retrieve().bodyToMono(Movie.class).block();
-*/
